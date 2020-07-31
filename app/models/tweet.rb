add6a04e39eb77b7ded0358ab7abac2826cbf275
shun-kubo8730 <bbpks080730@gmail.com>
2020-07-31 12:21:03 +0900

    incremental-search

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,6 +2,8 @@ class Tweet < ApplicationRecord
   validates :text, presence: true
   belongs_to :user
   has_many :comments
+  has_many :likes, dependent: :destroy
+  has_many :liking_users, through: :likes, source: :user
 
   def self.search(search)
     if search
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  post   '/like/:tweet_id' => 'likes#like',   as: 'like'
+  delete '/like/:tweet_id' => 'likes#unlike', as: 'unlike'
   devise_for :users
   get 'tweets/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
